8f2f20454061685ce17e0ed3718e7051ee771f17
Carlos Saucedo <carlos@cgsphoto.com>
2018-06-19 20:32:36 -0500

    Initial Commit.

---
+++ PDF_Parser/PDF_Parser.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Parser_Window
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ParseButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ParseButton
+        '
+        Me.ParseButton.Location = New System.Drawing.Point(232, 80)
+        Me.ParseButton.Name = "ParseButton"
+        Me.ParseButton.Size = New System.Drawing.Size(75, 23)
+        Me.ParseButton.TabIndex = 0
+        Me.ParseButton.Text = "Parse Files"
+        Me.ParseButton.UseVisualStyleBackColor = True
+        '
+        'Parser_Window
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(319, 115)
+        Me.Controls.Add(Me.ParseButton)
+        Me.Name = "Parser_Window"
+        Me.Text = "PDF_Parser"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ParseButton As Button
+End Class
---
+++ PDF_Parser/PDF_Parser.vb
@@ -0,0 +1,33 @@
+﻿Imports PdfSharp
+Imports PdfSharp.Drawing
+Imports PdfSharp.Pdf
+
+Module Parser
+
+    Sub Main()
+        'Creates a new PDF Document object.
+        Console.WriteLine("Initiating file parse.")
+        Dim parsedDoc As PdfDocument = New PdfDocument
+        parsedDoc.Info.Title = "Parsed Document"
+
+        'Adds a page for every image in the database.
+        'TODO add SQL database and image conversion.
+        Dim page As PdfPage = parsedDoc.AddPage
+
+        'Saves the created PDF document as a file and displays it.
+        Dim filename As String = "Doc.pdf"
+        parsedDoc.Save(filename)
+        Process.Start(filename)
+    End Sub
+
+End Module
+
+Public Class Parser_Window
+    Private Sub Parser_Window_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ParseButton.Click
+        Parser.Main()
+    End Sub
+End Class
