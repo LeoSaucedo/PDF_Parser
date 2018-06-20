a5172937c0f2503c946d959dc2d2485413811130
Carlos Saucedo <carlos@cgsphoto.com>
2018-06-20 15:06:14 -0500

    Began to write function to convert hex to XImage Object.

--- PDF_Parser/PDF_Parser.vb
+++ PDF_Parser/PDF_Parser.vb
@@ -15,6 +15,7 @@ Module Parser
 
         'Adds a page for every image in the specified directory.
         'As an example, a JPG and a PDF are included in the files/ directory.
+        'TODO add a way to convert binary data to a file.
         Dim dirPath As String = "files\"
         For Each i As String In Directory.GetFiles(dirPath) 'Retrieves the files inside of the specififed path.
             Dim newPage As PdfPage = parsedDoc.AddPage
@@ -33,13 +34,18 @@ End Module
 
 Public Class Parser_Window
     Inherits System.Windows.Forms.Form
-    'Crear objetos de ADO.NET.
+    'Create ADO.NET objects.
     Private Conn As SqlConnection
     Private Cmd As SqlCommand
     Private Reader As SqlDataReader
     Private results As String
 
     Private Sub Parser_Window_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Create a Connection object.
+        Conn = New SqlConnection() 'Insert connection details here.
+        'Create a Command object.
+        Cmd = Conn.CreateCommand
+        Cmd.CommandText = "SELECT varbinary from FILES" 'Default/sample database columbn with binary files.
 
     End Sub
 
